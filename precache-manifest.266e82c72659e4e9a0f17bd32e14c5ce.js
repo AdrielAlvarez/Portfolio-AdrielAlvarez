7b9904d3002ae5f1ca7a88a5f71d19d4d2554f50
School - Adriel Alvarez MacBook Pro <school-adrielalvarezmacbookpro@AllPurprielsMBP.localdomain>
2019-04-18 14:21:11 -0400

    Updates

---
+++ precache-manifest.266e82c72659e4e9a0f17bd32e14c5ce.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f77305bd819ba950830b",
+    "url": "/AdrielAlvarez/Portfolio-AdrielAlvarez/static/css/main.aba0e935.chunk.css"
+  },
+  {
+    "revision": "f77305bd819ba950830b",
+    "url": "/AdrielAlvarez/Portfolio-AdrielAlvarez/static/js/main.887f3cd4.chunk.js"
+  },
+  {
+    "revision": "e335c1d3ae73655fd10e",
+    "url": "/AdrielAlvarez/Portfolio-AdrielAlvarez/static/js/runtime~main.9075aa5d.js"
+  },
+  {
+    "revision": "ebfc9c3183c1adfb9ea3",
+    "url": "/AdrielAlvarez/Portfolio-AdrielAlvarez/static/js/2.f0259060.chunk.js"
+  },
+  {
+    "revision": "46156ced18e6ec44aaab5adc11aa5220",
+    "url": "/AdrielAlvarez/Portfolio-AdrielAlvarez/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/AdrielAlvarez/Portfolio-AdrielAlvarez.git/precache-manifest.7d8e84839f6fb850e69a7a2542df87ad.js"
+  "/AdrielAlvarez/Portfolio-AdrielAlvarez/precache-manifest.266e82c72659e4e9a0f17bd32e14c5ce.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/AdrielAlvarez/Portfolio-AdrielAlvarez.git/index.html", {
+workbox.routing.registerNavigationRoute("/AdrielAlvarez/Portfolio-AdrielAlvarez/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
