97ce435c39f4bedd21b6bd9d7fbb5489672968c5
School - Adriel Alvarez MacBook Pro <school-adrielalvarezmacbookpro@AllPurprielsMBP.localdomain>
2019-04-18 13:51:07 -0400

    Updates

---
+++ precache-manifest.ad0f5616df1fbdb072dbd6ec992fe964.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1456088e90e02fd5b6dc",
+    "url": "/AdrielAlvarez/Portfolio-AdrielAlvarez.git/static/css/main.caa88617.chunk.css"
+  },
+  {
+    "revision": "1456088e90e02fd5b6dc",
+    "url": "/AdrielAlvarez/Portfolio-AdrielAlvarez.git/static/js/main.1456088e.chunk.js"
+  },
+  {
+    "revision": "e990ccfaedfecdf9af24",
+    "url": "/AdrielAlvarez/Portfolio-AdrielAlvarez.git/static/js/1.e990ccfa.chunk.js"
+  },
+  {
+    "revision": "50ab8b5fa6e47572f912",
+    "url": "/AdrielAlvarez/Portfolio-AdrielAlvarez.git/static/js/runtime~main.50ab8b5f.js"
+  },
+  {
+    "revision": "922682369bde4ac03bccc343fda1a437",
+    "url": "/AdrielAlvarez/Portfolio-AdrielAlvarez.git/index.html"
+  }
+];
